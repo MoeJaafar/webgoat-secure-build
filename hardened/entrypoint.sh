fe6a154fafc3f4d751e97fb8874ca6ac25cc5208
Moe Jaafar <mohammad1999jaafar1999@gmail.com>
2025-05-03 16:50:53 +0300

    fixed ci

--- hardened/entrypoint.sh
+++ hardened/entrypoint.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-exec java \
-  -Dserver.address=0.0.0.0 \
-  -Dhibernate.hbm2ddl.auto=none \
-  -jar /webgoat/webgoat.jar
+exec java -Dserver.address=0.0.0.0 -Dhibernate.hbm2ddl.auto=none -jar /webgoat/webgoat.jar
